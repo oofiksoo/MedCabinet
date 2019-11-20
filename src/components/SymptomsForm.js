9fb10080f9d095438c31c26d669614cac151b895
Joseph Graham <Joseph@Chasingthefiks.net>
2019-11-20 13:28:05 -0700

    MVP-Met

--- src/components/SymptomsForm.js
+++ src/components/SymptomsForm.js
@@ -44,7 +44,12 @@ const SymptomsForm = ({ values, errors, touched, status }) => {
   const [recommendation2, setRecommendation2] = useState([]);
   const [symptom2, setSymptom2] = useState();
   const [symptom, setSymptom] = useState();
-
+  const [recommendation3, setRecommendation3] = useState([]);
+  const [recommendation4, setRecommendation4] = useState([]);
+  const [symptom3, setSymptom3] = useState();
+  const [symptom4, setSymptom4] = useState();
+  const [symptom5, setSymptom5] = useState();
+  const [recommendation5, setRecommendation5] = useState([]);
   useEffect(() => {
     const getRecommendation = () => {
       axios
@@ -83,6 +88,63 @@ const SymptomsForm = ({ values, errors, touched, status }) => {
     };
     getRecommendation2();
   }, [symptom2]);
+  useEffect(() => {
+    const getRecommendation3 = () => {
+      axios
+
+        .get(
+          `https://strainapi.evanbusse.com/VUGyzwt/strains/search/effect/${symptom3}`
+        )
+
+        .then(response => {
+          setRecommendation3(response.data);
+          console.log(response);
+        })
+
+        .catch(error => {
+          console.error("Server Error", error);
+        });
+    };
+    getRecommendation3();
+  }, [symptom3]);
+  useEffect(() => {
+    const getRecommendation4 = () => {
+      axios
+
+        .get(
+          `https://strainapi.evanbusse.com/VUGyzwt/strains/search/effect/${symptom4}`
+        )
+
+        .then(response => {
+          setRecommendation4(response.data);
+          console.log(response);
+        })
+
+        .catch(error => {
+          console.error("Server Error", error);
+        });
+    };
+    getRecommendation4();
+  }, [symptom4]);
+  useEffect(() => {
+    const getRecommendation5 = () => {
+      axios
+
+        .get(
+          `https://strainapi.evanbusse.com/VUGyzwt/strains/search/effect/${symptom5}`
+        )
+
+        .then(response => {
+          setRecommendation5(response.data);
+          console.log(response);
+        })
+
+        .catch(error => {
+          console.error("Server Error", error);
+        });
+    };
+    getRecommendation5();
+  }, [symptom5]);
   return (
     <Form>
       <SQuestionCont>
@@ -159,6 +221,135 @@ const SymptomsForm = ({ values, errors, touched, status }) => {
         <p>Recommendations:</p>
         <RecContainer>
           {recommendation2.slice(11, 16).map(strain => {
+            return (
+              <RecommendationCard
+                key={strain.id}
+                sName={strain.name}
+                race={strain.race}
+                id={strain.id}
+              />
+            );
+          })}
+        </RecContainer>
+      </SQuestionCont>
+      <hr></hr>
+      <SQuestionCont>
+        <QCont>
+          <p> Select Symptom 3: </p>
+          <Field
+            as="select"
+            className="Symptom"
+            name="Symptom3"
+            onChange={e => {
+              setSymptom3(e.target.value);
+              console.log(e.target.value);
+            }}
+          >
+            <option value="">Select A Symptom</option>
+            <option value="Depression">Depression</option>
+            <option value="Stress">Stress</option>
+            <option value="Insomnia">Insomnia</option>
+            <option value="Pain">Pain</option>
+            <option value="Muscle Spasms">Muscle Spasms</option>
+            <option value="Inflammation">Inflammation</option>
+            <option value="Fatigue">Fatigue</option>
+            <option value="Hungry">Lack of Appitite</option>
+            <option value="Seizures">Seizures</option>
+            <option value="Cramps">Cramps</option>
+          </Field>
+          {touched.Symptom3 && errors.Symptom3 && (
+            <p className="error-display"> {errors.Symptom3} </p>
+          )}
+        </QCont>
+        <p>Recommendations:</p>
+        <RecContainer>
+          {recommendation3.slice(11, 16).map(strain => {
+            return (
+              <RecommendationCard
+                key={strain.id}
+                sName={strain.name}
+                race={strain.race}
+                id={strain.id}
+              />
+            );
+          })}
+        </RecContainer>
+      </SQuestionCont>
+      <hr></hr>
+      <SQuestionCont>
+        <QCont>
+          <p> Select Symptom 4: </p>
+          <Field
+            as="select"
+            className="Symptom"
+            name="Symptom4"
+            onChange={e => {
+              setSymptom4(e.target.value);
+              console.log(e.target.value);
+            }}
+          >
+            <option value="">Select A Symptom</option>
+            <option value="Depression">Depression</option>
+            <option value="Stress">Stress</option>
+            <option value="Insomnia">Insomnia</option>
+            <option value="Pain">Pain</option>
+            <option value="Muscle Spasms">Muscle Spasms</option>
+            <option value="Inflammation">Inflammation</option>
+            <option value="Fatigue">Fatigue</option>
+            <option value="Hungry">Lack of Appitite</option>
+            <option value="Seizures">Seizures</option>
+            <option value="Cramps">Cramps</option>
+          </Field>
+          {touched.Symptom4 && errors.Symptom4 && (
+            <p className="error-display"> {errors.Symptom4} </p>
+          )}
+        </QCont>
+        <p>Recommendations:</p>
+        <RecContainer>
+          {recommendation4.slice(11, 16).map(strain => {
+            return (
+              <RecommendationCard
+                key={strain.id}
+                sName={strain.name}
+                race={strain.race}
+                id={strain.id}
+              />
+            );
+          })}
+        </RecContainer>
+      </SQuestionCont>
+      <hr></hr>
+      <SQuestionCont>
+        <QCont>
+          <p> Select Symptom 5: </p>
+          <Field
+            as="select"
+            className="Symptom"
+            name="Symptom5"
+            onChange={e => {
+              setSymptom5(e.target.value);
+              console.log(e.target.value);
+            }}
+          >
+            <option value="">Select A Symptom</option>
+            <option value="Depression">Depression</option>
+            <option value="Stress">Stress</option>
+            <option value="Insomnia">Insomnia</option>
+            <option value="Pain">Pain</option>
+            <option value="Muscle Spasms">Muscle Spasms</option>
+            <option value="Inflammation">Inflammation</option>
+            <option value="Fatigue">Fatigue</option>
+            <option value="Hungry">Lack of Appitite</option>
+            <option value="Seizures">Seizures</option>
+            <option value="Cramps">Cramps</option>
+          </Field>
+          {touched.Symptom2 && errors.Symptom4 && (
+            <p className="error-display"> {errors.Symptom4} </p>
+          )}
+        </QCont>
+        <p>Recommendations:</p>
+        <RecContainer>
+          {recommendation5.slice(11, 16).map(strain => {
             return (
               <RecommendationCard
                 key={strain.id}
