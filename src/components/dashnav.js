ca03a49cf453148d1104ac8cdd9edbb8ed8b943c
Joseph Graham <Joseph@Chasingthefiks.net>
2019-11-20 13:13:55 -0700

    MVP-Met

--- src/components/dashnav.js
+++ src/components/dashnav.js
@@ -7,20 +7,20 @@ import Symptoms from "./symptoms";
 import Recomendations from "./recomendations";
 import Strains from "./strains";
 
-const Dashboard = styled.div`
+const Dashboard = styled.div `
   display: flex;
 `;
 
-const DashNavContainer = styled.div`
+const DashNavContainer = styled.div `
   display: flex;
   flex-direction: column;
   align-items: space-between;
   background-color: #058a17;
-  width: 11vw;
+  width: 12vw;
   border-bottom-right-radius: 50px;
   min-height: 75vh;
 `;
-const DashNavItem = styled.button`
+const DashNavItem = styled.button `
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -41,46 +41,62 @@ const DashNavItem = styled.button`
 `;
 
 export default function DashNav() {
-  const [strainToSearch, setStrainToSearch] = useState();
-  return (
-    <Dashboard>
-      <DashNavContainer>
-        <NavLink exact to="/">
-          <DashNavItem>
-            <i className="fas fa-home fa-3x"> </i> <p> Home </p>{" "}
-          </DashNavItem>{" "}
-        </NavLink>{" "}
-        <NavLink to="/symptoms">
-          <DashNavItem>
-            <i className="fas fa-notes-medical fa-3x"> </i>{" "}
-            <p> Recommendations by Symptom </p>{" "}
-          </DashNavItem>{" "}
-        </NavLink>{" "}
-        <NavLink to="/recomendations">
-          <DashNavItem>
-            <i className="fas fa-prescription-bottle-alt fa-3x"> </i>{" "}
-            <p> My Recomendations </p>{" "}
-          </DashNavItem>{" "}
-        </NavLink>{" "}
-        <NavLink to="/strains">
-          <DashNavItem>
-            <i className="fas fa-cannabis fa-3x"> </i> <p> Browse Strains </p>{" "}
-          </DashNavItem>{" "}
-        </NavLink>{" "}
-      </DashNavContainer>{" "}
-      <Route exact path="/" component={WelcomePage} />{" "}
-      <Route path="/symptoms" component={Symptoms} />{" "}
-      <Route path="/recomendations" component={Recomendations} />{" "}
-      <Route
-        path="/strains"
-        render={props => (
-          <Strains
-            {...props}
-            strainToSearch={strainToSearch}
-            setStrainToSearch={setStrainToSearch}
-          />
-        )}
-      />{" "}
-    </Dashboard>
-  );
+    const [strainToSearch, setStrainToSearch] = useState();
+    return ( <
+        Dashboard >
+        <
+        DashNavContainer >
+        <
+        NavLink exact to = "/" >
+        <
+        DashNavItem >
+        <
+        i className = "fas fa-home fa-3x" > < /i> <p> Home </p > { " " } <
+        /DashNavItem>{" "} <
+        /NavLink>{" "} <
+        NavLink to = "/symptoms" >
+        <
+        DashNavItem >
+        <
+        i className = "fas fa-notes-medical fa-3x" > < /i>{" "} <
+        p > Recommendations by Symptom < /p>{" "} <
+        /DashNavItem>{" "} <
+        /NavLink>{" "} <
+        NavLink to = "/recomendations" >
+        <
+        DashNavItem >
+        <
+        i className = "fas fa-prescription-bottle-alt fa-3x" > < /i>{" "} <
+        p > My Recomendations < /p>{" "} <
+        /DashNavItem>{" "} <
+        /NavLink>{" "} <
+        NavLink to = "/strains" >
+        <
+        DashNavItem >
+        <
+        i className = "fas fa-cannabis fa-3x" > < /i> <p> Browse Strains </p > { " " } <
+        /DashNavItem>{" "} <
+        /NavLink>{" "} <
+        /DashNavContainer>{" "} <
+        Route exact path = "/"
+        component = { WelcomePage }
+        />{" "} <
+        Route path = "/symptoms"
+        component = { Symptoms }
+        />{" "} <
+        Route path = "/recomendations"
+        component = { Recomendations }
+        />{" "} <
+        Route path = "/strains"
+        render = {
+            props => ( <
+                Strains {...props }
+                strainToSearch = { strainToSearch }
+                setStrainToSearch = { setStrainToSearch }
+                />
+            )
+        }
+        />{" "} <
+        /Dashboard>
+    );
 }
